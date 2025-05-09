cf11b5b8e4b1bfe4c9e26c876eaf8f7cbecb67d9
KukkikD <adeezon9@gmail.com>
2025-05-09 01:33:33 -0700

    add

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use('/professional', professionalRoutes);
+
+app.listen(8080);
