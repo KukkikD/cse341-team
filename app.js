26840698efedd0072f3bd7c0313828b5f3c031da
KukkikD <adeezon9@gmail.com>
2025-05-09 02:35:31 -0700

    update

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+/* middleware */
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*'); //header CORS (Cross-Origin Resource Sharing) : Allow all domains to access this API.
   next();
 });
 
+/* route */
 app.use('/professional', professionalRoutes);
 
+
+/* server */
 app.listen(8080);
